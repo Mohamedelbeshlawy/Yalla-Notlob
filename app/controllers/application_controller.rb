6665d898d4babbca7a079c15e0e4b88aea21d325
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-04-20 07:43:13 +0200

    continue authentication sign up & login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # before_action :authenticate_user!
+
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :first_name, :last_name])
+  end
+
+  protected
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_path
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
+    end
+  end
 end
