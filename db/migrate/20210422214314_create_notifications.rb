24de65dcb7259ab698d44822886279d3c3c8d0d5
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-04-23 00:03:26 +0200

    continue fix add friend bug

--- db/migrate/20210422214314_create_notifications.rb
+++ db/migrate/20210422214314_create_notifications.rb
@@ -1,8 +1,16 @@
 class CreateNotifications < ActiveRecord::Migration[6.1]
   def change
     create_table :notifications do |t|
+      t.bigint :recipient_id
+      t.bigint :actor_id
+      t.datetime :read_at
+      t.string :action
+      t.string :model
+      #t.references :notifiable, polymorphic: true
 
       t.timestamps
     end
+    add_foreign_key :notifications, :users, column: :recipient_id
+    add_foreign_key :notifications, :users, column: :actor_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_20_214527) do
+ActiveRecord::Schema.define(version: 2021_04_22_214314) do
 
   create_table "friendships", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id"
@@ -19,6 +19,23 @@ ActiveRecord::Schema.define(version: 2021_04_20_214527) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["friend_id"], name: "fk_rails_d78dc9c7fd"
     t.index ["user_id"], name: "fk_rails_e3733b59b7"
+  end
+
+  create_table "invites", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notifications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "recipient_id"
+    t.bigint "actor_id"
+    t.datetime "read_at"
+    t.string "action"
+    t.string "model"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["actor_id"], name: "fk_rails_06a39bb8cc"
+    t.index ["recipient_id"], name: "fk_rails_4aea6afa11"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -37,4 +54,6 @@ ActiveRecord::Schema.define(version: 2021_04_20_214527) do
 
   add_foreign_key "friendships", "users"
   add_foreign_key "friendships", "users", column: "friend_id"
+  add_foreign_key "notifications", "users", column: "actor_id"
+  add_foreign_key "notifications", "users", column: "recipient_id"
 end
