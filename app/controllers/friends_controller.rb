109a21fe3d35f5abe9ef7e8f345a28ff4114ed7d
Ahmed Ibrahim <ahmedibrahem22322@gmail.com>
2021-04-28 01:19:14 +0200

    test the new friendship edit

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -8,11 +8,7 @@ class FriendsController < ApplicationController
     end
   end
 
-  def new
-    @new_friendship = current_user.friendship.new()
-    @errors = Array.new
-
-  end
+  
 
   def create
     @errors = Array.new
