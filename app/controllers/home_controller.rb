9d15c0936a03e39754739f9885a2f1092bb10c8c
nagyadel1992 <nagy.adel10@yahoo.com>
2021-04-28 03:06:20 +0200

    editing HomePageController to be vailed with invitation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,19 @@
 class HomeController < ApplicationController
+    before_action :authenticate_user!
+
     def index
+      orders = Order.where(user_id: current_user.id)
+      invites = Invite.where(user_id: current_user.id)
+      @orders_from_invitations = []
+      invites.each do |invite|
+        @orders_from_invitations.append(Order.where(id: invite.order_id).first)
+      end
+  
+      
+      @orders_from_invitations = @orders_from_invitations[0..4]
     end
+  
+    def show
+    end
+  
 end
