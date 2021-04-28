e8d85e73eca93a0819b22f7fe5696c1f4e6de671
nagyadel1992 <nagy.adel10@yahoo.com>
2021-04-28 18:03:06 +0200

    Fixing Auth Bugs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
     def index
       orders = Order.where(user_id: current_user.id)
-      invites = Invite.where(user_id: current_user.id)
+      invites = Invitation.where(user_id: current_user.id)
       @orders_from_invitations = []
       invites.each do |invite|
         @orders_from_invitations.append(Order.where(id: invite.order_id).first)
